c47346ec98efb9f7ec3f0ac79a9c1a30659333de
Emily Read <emilyread01@gmail.com>
2021-07-13 13:10:11 -0700

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[poke, setPoke] = useState([])
+
+  // useEffect(()=>{
+  //   axios.get('https://pokeapi.co/api/v2/pokemon?limit=807').then(response=>{setPoke(response.data.results)}, [])
+  //   console.log(poke)
+  // }
+
+  // )
+  const fetchpoke=()=>{
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807').then(response=>{setPoke(response.data.results)})
+      console.log(poke)
+    }
+
+    return(
+      <div>
+        <h1> Pokemon </h1>
+        <button onClick={fetchpoke}>Fetch Pokemon</button>
+        {poke.length>0&& poke.map((poke, index)=>{
+          return(
+            <div key={index}>
+              <ul>
+                <li>{poke.name}</li>
+              </ul>
+            </div>
+          )
+        })}
+      </div>
+    )
 }
 
 export default App;
